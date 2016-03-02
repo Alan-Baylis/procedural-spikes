b298c8370d1f8adec12e8999f53f46a404c040a6
Tencho Tenev <tenev.dev@outlook.com>
2016-03-02 22:45:53 +0000

    Add classes for working with polar coordinates

---
+++ Spikes/Assets/Scripts/Geometry/PolarSystem.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Assets.Scripts.Geometry
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class PolarSystem
+    {
+        // Should only allow a single set of the pole which happens in the constructor by design.
+        // This is to ensure that the relative positions of all other vectors do not have to be
+        // recomputed if the pole is changed.
+        private readonly PolarVector pole;
+
+        public PolarSystem(PolarVector pole)
+        {
+            this.pole = pole;
+        }
+
+        /// <summary>
+        /// The centre of the coordinate system. Can only be set through the constructor.
+        /// </summary>
+        public PolarVector Pole
+        {
+            get
+            {
+                return pole;
+            }
+        }
+
+        /// <summary>
+        /// Generate a random position on a circle
+        /// </summary>
+        /// <param name="radius">Distance from pole</param>
+        /// <returns>A polar vector at the specified distance and random angle</returns>
+        public PolarVector GetPointOnCircle(float radius)
+        {
+            return new PolarVector(radius, Random.Range(0, 2 * Mathf.PI), Pole);
+        }
+
+        /// <summary>
+        /// Generate a random position in a segment of a circle
+        /// </summary>
+        /// <param name="radius">Distance from pole</param>
+        /// <param name="fromAzimuth">Lower angle of segment to include</param>
+        /// <param name="toAzimuth">Higher angle of segment to include</param>
+        /// <returns>A polar vector which lies between the two given angles at the specified distance</returns>
+        public PolarVector GetPointOnCircle(float radius, float fromAzimuth, float toAzimuth)
+        {
+            return new PolarVector(radius, Random.Range(fromAzimuth, toAzimuth), Pole);
+        }
+
+        /// <summary>
+        /// Generate a random position on a circle inside a segment range relative to another vector.
+        /// The segment is determined by adding the given range on each side of the azimuth of the given vector.
+        /// </summary>
+        /// <param name="radius">Distance from pole</param>
+        /// <param name="azimuthRange">Magnitude of segment range in each direction (in radians)</param>
+        /// <param name="relativeTo">A vector whose azimuth is used as the centre of the segment</param>
+        /// <returns></returns>
+        public PolarVector GetPointOnCircle(float radius, float azimuthRange, PolarVector relativeTo)
+        {
+            return new PolarVector(radius, Random.Range(relativeTo.Azimuth - azimuthRange, relativeTo.Azimuth + azimuthRange), Pole);
+        }
+    }
+}
---
+++ Spikes/Assets/Scripts/Geometry/PolarVector.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts.Geometry
+{
+    /// <summary>
+    /// I have decided to keep things simple. This is a straightforward
+    /// implementation of polar vector and the only clever thing I am doing is
+    /// computing the cartesian representation in advance because I know that I
+    /// will need it at some point (probably multiple times). If you want to
+    /// optimise it you should probably consider making it a struct and
+    /// applying some problem-specific knowledge.
+    /// </summary>
+    public class PolarVector
+    {
+        public PolarVector(float radius, float azimuth)
+        {
+            Radius = radius;
+            Azimuth = azimuth;
+
+            Cartesian = ToCartesianVector();
+        }
+
+        public PolarVector(float radius, float azimuth, PolarVector pole)
+        {
+            Radius = radius;
+            Azimuth = azimuth;
+
+            Cartesian = ToCartesianVector();
+            WorldCartesian = ToCartesianVector(pole);
+        }
+
+        public PolarVector(Vector3 cartesianVector)
+        {
+            Radius = Mathf.Sqrt(cartesianVector.sqrMagnitude);
+            Azimuth = Mathf.Atan2(cartesianVector.z, cartesianVector.x);
+
+            Cartesian = cartesianVector;
+            WorldCartesian = cartesianVector;
+        }
+
+        public float Radius { get; set; }
+        public float Azimuth { get; set; }
+        public Vector3 Cartesian { get; protected set; }
+        public Vector3 WorldCartesian { get; protected set; }
+
+        /// <summary>
+        /// Used for constructing the mesh.
+        /// This property would not usually end up in a generic polar vector
+        /// class but I have decided to put it here to keep the class hierarchy
+        /// simpler. I recommend creating a mesh vertex class which holds a
+        /// property to the position (a polar vector) and the vertex index.
+        /// </summary>
+        public int VertexIndex { get; set; }
+
+        private Vector3 ToCartesianVector()
+        {
+            return new Vector3(Radius * Mathf.Cos(Azimuth), 0, Radius * Mathf.Sin(Azimuth));
+        }
+
+        /// <summary>
+        /// Get a vector if the pole of a polar system is not the same as the Unity world origin
+        /// </summary>
+        /// <param name="pole">The centre of the polar coordinate system used to work with this vector</param>
+        /// <returns>A cartesian vector representation of this vector relative to the Unity world origin</returns>
+        private Vector3 ToCartesianVector(PolarVector pole)
+        {
+            return pole.Cartesian + Cartesian;
+        }
+
+    }
+}
